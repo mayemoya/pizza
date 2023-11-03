a47bfa9431b6baac82368629fa9ac91b609eceb6
Fanny Paola Colindres Herrera <paolacolindres@yahoo.com>
2023-11-02 20:36:54 -0600

    agregue procesos para hacer pizza

---
+++ pizza.py
@@ -0,0 +1,7 @@
+#Hacer una Pizza
+#Hacer la masa para pizza 
+    #revolver harina con la levadura, agua, sal y aceite de oliva
+    # Amasa y Dejar Reposar
+#Hacer la salsa al gusto
+#expandir la harina, poner salsa y jamon y vegetales
+#Meter al horno a 250 grados por 25 minutos
